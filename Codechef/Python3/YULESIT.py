d30324222f331753cf22a9c288293ef0ec814aba
Shubham Singh <enclose.info@gmail.com>
2020-02-17 23:16:25 +0530

    Delete YULESIT.py

--- Codechef/Python3/YULESIT.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-for j in range(int(input())):
-    n=int(input())
-    l=list(input().split())
-    d=dict(Counter(l))
-    for k in d.keys():
-        if d[k]==1:
-            print(int(k))
-            break
