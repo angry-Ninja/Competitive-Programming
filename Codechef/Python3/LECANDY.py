8cf880250aaee5041b8620f0183e2dc7ec47c64a
Shubham Singh <enclose.info@gmail.com>
2020-02-17 23:05:57 +0530

    Delete LECANDY.py

--- Codechef/Python3/LECANDY.py
+++
@@ -1,7 +0,0 @@
-for i in range(0,int(input())):
-    E,C=list(map(int,input().split()))
-    A=sum(list(map(int,input().split())))
-    if A<=C:
-        print("Yes")
-    else:
-        print("No")
