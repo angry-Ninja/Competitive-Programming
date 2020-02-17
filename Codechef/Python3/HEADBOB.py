a2ac8f75c22af4fbc49b0d52e558f08fb95ea439
Shubham Singh <enclose.info@gmail.com>
2020-02-17 23:03:12 +0530

    Delete HEADBOB.py

--- Codechef/Python3/HEADBOB.py
+++
@@ -1,9 +0,0 @@
-for j in range(int(input())):
-    n=int(input())
-    s=input()
-    if "I" in s:
-        print("INDIAN")
-    elif ("N" in s and "Y" in s) or ("N" not in s and "Y" in s) :
-        print("NOT INDIAN")
-    else:
-        print("NOT SURE")
