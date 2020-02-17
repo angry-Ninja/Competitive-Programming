2234b066567e912dfbff677de4808cf713d59071
Shubham Singh <enclose.info@gmail.com>
2020-02-17 23:12:35 +0530

    Delete QM16A.py

--- Codechef/Python3/QM16A.py
+++
@@ -1,8 +0,0 @@
-for _ in range(int(input())):
-    s1=set(input().split())
-    s2=set(input().split())
-    s3=s1.intersection(s2)
-    if len(s3)>0:
-        print("Yes")
-    else:
-        print("No")
